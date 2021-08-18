b740cf097406e0f51dd7c3c7d0d4a53000be5509
rosseab-bit <roseab.developer@gmail.com>
2021-08-18 05:55:12 -0300

    add new file

---
+++ appDesktop.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import json
+import sys
+import os
+import time
+from packages import readMarkers
+from packages import editMarkers
+from packages import writeMarkers
+from packages import dellMarkers
+
